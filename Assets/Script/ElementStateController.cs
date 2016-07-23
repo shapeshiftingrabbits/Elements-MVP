acd5754b2da300d6ed98bc43f17162f4b208283d
Laurie Trichet <laurie.trichet@gmail.com>
2016-07-23 20:24:19 +1000

    Add water space on the plane. Add State to the player.

---
+++ Assets/Script/ElementStateController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElementStateController : MonoBehaviour {
+
+	public enum State {
+		Water,
+		Normal
+	}
+
+	public State currentState;
+
+	// Use this for initialization
+	void Start () {
+		SetState(State.Normal);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void SetState(State newState){
+		currentState = newState;
+		UpdateState ();
+	}
+
+	void UpdateState(){
+		Renderer renderer = GetComponent<Renderer> ();
+		if (currentState.Equals (State.Normal)) {
+			renderer.material.color = Color.green;
+		} else {
+			renderer.material.color = Color.blue;
+		}
+	}
+
+
+
+}
