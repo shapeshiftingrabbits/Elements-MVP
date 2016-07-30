0d0212994d651ee50e7f4d357efa91552ee87201
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-07-30 22:11:45 +1000

    Melt ice progressively when player is close and on fire

--- Assets/HoldMechanic/Scripts/IceMeltScript.cs
+++ Assets/HoldMechanic/Scripts/IceMeltScript.cs
@@ -4,19 +4,32 @@ using System.Collections;
 public class IceMeltScript : MonoBehaviour {
 
 	private float startingResistance = 1.0f;
-	public float currentResistance;
+	public float currentResistance = 1.0f;
+	private float meltAmout = 0.1f;
+	private float meltTick = 0.5f;
+	private float elapsedTime = 0f;
+	private Transform originalTransform;
 
 	// Use this for initialization
 	void Start () {
 		currentResistance = startingResistance;
+		originalTransform = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.localScale = transform.localScale * currentResistance;
+		
 	}
 
-	void Melt() {
-		currentResistance -= 0.1f;
+	public void Melt(float deltaTime) {
+		elapsedTime += deltaTime;
+
+		if (elapsedTime >= meltTick) {
+			currentResistance -= meltAmout;
+			elapsedTime = 0f;
+
+			Transform newTransform = originalTransform;
+			newTransform.localScale = newTransform.localScale * currentResistance;
+		}
 	}
 }
---
+++ Assets/HoldMechanic/Scripts/PlayerFireIceMeltScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (PlayerElementControllerScript))]
+[RequireComponent (typeof (CapsuleCollider))]
+public class PlayerFireIceMeltScript : MonoBehaviour {
+
+	private PlayerElementControllerScript elementControllerScript;
+	private CapsuleCollider capsuleCollider;
+	private LayerMask iceLayer;
+
+	// Use this for initialization
+	void Start () {
+		elementControllerScript = GetComponent<PlayerElementControllerScript> ();
+		iceLayer = LayerMask.NameToLayer ("Ice");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerStay (Collider other) {
+		if (elementControllerScript.currentElement == PlayerElementControllerScript.Element.Fire) {
+			if (other.gameObject.layer == iceLayer) {
+				IceMeltScript iceMeltScript = other.gameObject.GetComponent<IceMeltScript> ();
+				iceMeltScript.Melt (Time.deltaTime);
+			}
+		}
+	}
+}
