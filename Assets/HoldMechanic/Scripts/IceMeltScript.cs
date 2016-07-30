a4fb021c22b406a21456d79b750c28bb0b51a80f
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-07-30 19:42:31 +1000

    Create Ice prefab and add a few blocks to the scene

---
+++ Assets/HoldMechanic/Scripts/IceMeltScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceMeltScript : MonoBehaviour {
+
+	private float startingResistance = 1.0f;
+	public float currentResistance;
+
+	// Use this for initialization
+	void Start () {
+		currentResistance = startingResistance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = transform.localScale * currentResistance;
+	}
+
+	void Melt() {
+		currentResistance -= 0.1f;
+	}
+}
