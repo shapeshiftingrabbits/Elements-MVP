01cde746afee609fe08a67a2de8fd3b30ffce569
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-08-10 18:57:40 +1000

    Reload scene when player is within the end level object

---
+++ Assets/HoldMechanic/Scripts/LevelExitScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent (typeof(Collider))]
+public class LevelExitScript : MonoBehaviour {
+
+	Collider levelEndCollider;
+	LayerMask playerLayer;
+
+	void Start () {
+		levelEndCollider = gameObject.GetComponent<Collider>();
+		playerLayer = LayerMask.NameToLayer ("Player");
+	}
+
+	void OnTriggerStay (Collider other) {
+		if (other.gameObject.layer == playerLayer && playerIsWithinBounds(other)) {
+			SceneManager.LoadScene ("MainScene");
+		}
+	}
+
+	bool playerIsWithinBounds (Collider playerCollider) {
+		return (playerIsWithinX(playerCollider) && playerIsWithinZ(playerCollider));
+	}
+
+	bool playerIsWithinX(Collider playerCollider) {
+		return playerCollider.bounds.min.x >= levelEndCollider.bounds.min.x && playerCollider.bounds.max.x <= levelEndCollider.bounds.max.x;
+	}
+
+	bool playerIsWithinZ(Collider playerCollider) {
+		return playerCollider.bounds.min.z >= levelEndCollider.bounds.min.z && playerCollider.bounds.max.z <= levelEndCollider.bounds.max.z;
+	}
+}
