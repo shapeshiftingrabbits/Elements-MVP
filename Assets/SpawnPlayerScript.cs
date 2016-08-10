79172452abdc69a49f2026988f1b4e5aa061e349
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-08-10 17:54:11 +1000

    Write script to spawn player on level start

---
+++ Assets/SpawnPlayerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlayerScript : MonoBehaviour {
+
+	public GameObject levelStart;
+	public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (playerPrefab, playerSpawnPosition (), Quaternion.identity);
+	}
+
+	Vector3 playerSpawnPosition() {
+		return levelStart.transform.position;
+	}
+}
